6acef14271e17cd7b527ef6b865f0db0ee3083f0
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-04-27 15:33:04 +0200

    Updated LoggingOptions.cs with null!

--- Platform/Options/LoggingOptions.cs
+++ Platform/Options/LoggingOptions.cs
@@ -4,7 +4,7 @@ namespace Platform.Options;
 
 public class LoggingOptions : AbstractOptions
 {
-    public string ApplicationName { get; init; }
+    public string ApplicationName { get; init; } = null!;
 
     public LoggingOptions(IConfiguration configuration) : base(configuration)
     {
